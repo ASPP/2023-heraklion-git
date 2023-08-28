7ea013392c5588013353b6adac68b32437363af7
Tiziano Zito <opossumnano@gmail.com>
2023-08-28 12:26:54 +0300

    solution stub

---
+++ auth.py
@@ -0,0 +1,32 @@
+import json
+import getpass
+
+PATH = "./pwdb.json"
+
+def get_credentials():
+    username = input("Username: ")
+    password = getpass.getpass("Password: ")
+    return username, password
+
+def read_pwdb():
+    with open(PATH, "rt") as f:
+        pwdb = json.load(f)
+    return pwdb
+
+def write_pwdb(username, password):
+    pwdb = {username: password}
+    with open(PATH, "w") as f:
+        json.dump(pwdb, f)
+
+
+def authenticate(username, password, pwdb):
+    if username in pwdb:
+        if password == pwdb[username]:
+            print("Successfully authenticated!")
+
+
+username, password = get_credentials()
+write_pwdb(username, password)
+pwdb = read_pwdb()
+authenticate(username, password, pwdb)
+
